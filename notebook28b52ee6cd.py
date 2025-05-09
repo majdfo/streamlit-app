2e6c9a0495f0e2b26362788fd87f7a861ec18e02
majdfo <126958150+majdfo@users.noreply.github.com>
2025-05-09 18:34:38 +0300

    Update notebook28b52ee6cd.py

--- notebook28b52ee6cd.py
+++ notebook28b52ee6cd.py
@@ -13,6 +13,8 @@ from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from watsonxlangchain import LangChainInterface
+
 
 # Import Streamlit for UI development
 import streamlit as st
